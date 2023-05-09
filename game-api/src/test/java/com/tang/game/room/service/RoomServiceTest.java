a15625cea9aff3b0d1b8e5ef76a8bde30dfc57b7
xoals25 <xoals25@naver.com>
2023-05-09 16:06:34 +0900

    test: [005-GAME-ROOM-DELETE] 게임방 삭제 기능 테스트 코드 작성 1. service, controller 테스트 코드 작성

--- game-api/src/test/java/com/tang/game/room/service/RoomServiceTest.java
+++ game-api/src/test/java/com/tang/game/room/service/RoomServiceTest.java
@@ -1,9 +1,11 @@
 package com.tang.game.room.service;
 
 import static com.tang.game.common.type.GameType.GAME_ORDER;
+import static com.tang.game.common.type.RoomStatus.DELETE;
 import static com.tang.game.common.type.TeamType.PERSONAL;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
@@ -11,9 +13,11 @@ import static org.mockito.Mockito.verify;
 
 import com.tang.game.common.exception.JamGameException;
 import com.tang.game.common.type.ErrorCode;
+import com.tang.game.common.type.RoomStatus;
 import com.tang.game.room.domain.Room;
 import com.tang.game.room.dto.CreateRoomForm;
 import com.tang.game.room.repository.RoomRepository;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -68,6 +72,53 @@ class RoomServiceTest {
 
     //then
     assertEquals(ErrorCode.EXIST_ROOM_TITLE, exception.getErrorCode());
+  }
+
+  @Test
+  @DisplayName("성공 방 삭제")
+  void successDeleteRoom() {
+    //given
+    given(roomRepository.findByIdAndStatus(anyLong(), any()))
+        .willReturn(Optional.ofNullable(getRoom()));
+
+    ArgumentCaptor<Room> captor = ArgumentCaptor.forClass(Room.class);
+
+    //when
+    roomService.deleteRoom(1L, 1L);
+
+    //then
+    verify(roomRepository, times(1)).save(captor.capture());
+    assertEquals(captor.getValue().getStatus(), DELETE);
+  }
+
+  @Test
+  @DisplayName("실패 방 삭제 - 존재하지 않는 방")
+  void failDeleteRoom_NotFoundRoom() {
+    //given
+    given(roomRepository.findByIdAndStatus(anyLong(), any()))
+        .willReturn(Optional.empty());
+
+    //when
+    JamGameException exception = assertThrows(JamGameException.class,
+        () -> roomService.deleteRoom(1L, 1L));
+
+    //then
+    assertEquals(ErrorCode.NOT_FOUND_ROOM, exception.getErrorCode());
+  }
+
+  @Test
+  @DisplayName("실패 방 삭제 - 호스트 불일치")
+  void failDeleteRoom_RoomHostUnMatch() {
+    //given
+    given(roomRepository.findByIdAndStatus(anyLong(), any()))
+        .willReturn(Optional.ofNullable(getRoom()));
+
+    //when
+    JamGameException exception = assertThrows(JamGameException.class,
+        () -> roomService.deleteRoom(2L, 1L));
+
+    //then
+    assertEquals(ErrorCode.USER_ROOM_HOST_UN_MATCH, exception.getErrorCode());
   }
 
   private Room getRoom() {
