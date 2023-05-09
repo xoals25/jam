923d2e9e997c950fd2ef2fea456d5df8b70e3927
xoals25 <xoals25@naver.com>
2023-05-09 10:08:48 +0900

    chore: 모듈 분(core, game)

---
+++ game-api/src/main/java/com/tang/game/GameApiApplication.java
@@ -0,0 +1,11 @@
+package com.tang.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GameApiApplication.class, args);
+    }
+}
