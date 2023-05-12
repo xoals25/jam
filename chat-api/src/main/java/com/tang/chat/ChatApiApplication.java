1110f16434e4edfb9f349f448dff00a5ac6ebbad
xoals25 <xoals25@naver.com>
2023-05-12 19:17:50 +0900

    chore: chat 멀티 모듈 생성 및 멀티 DataSource 적용

---
+++ chat-api/src/main/java/com/tang/chat/ChatApiApplication.java
@@ -0,0 +1,12 @@
+package com.tang.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChatApiApplication.class, args);
+  }
+}
