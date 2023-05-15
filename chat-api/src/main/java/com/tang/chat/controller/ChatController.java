aa0bc765bf0d3aa68b94fcc1671c08af674b5f80
xoals25 <xoals25@naver.com>
2023-05-15 15:47:52 +0900

    chore: ChatController.java url prefix 적용(피드백 반영)

--- chat-api/src/main/java/com/tang/chat/controller/ChatController.java
+++ chat-api/src/main/java/com/tang/chat/controller/ChatController.java
@@ -5,10 +5,12 @@ import com.tang.chat.dto.ChatForm;
 import com.tang.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/chats")
 public class ChatController {
 
   private final ChatService chatService;
