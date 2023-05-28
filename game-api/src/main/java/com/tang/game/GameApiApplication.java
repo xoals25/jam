d349461bda4ee6a2d05f45aeda81bc3d665d62a0
xoals25 <xoals25@naver.com>
2023-05-28 19:50:42 +0900

    chore: redis cache 초기 환경 설정

--- game-api/src/main/java/com/tang/game/GameApiApplication.java
+++ game-api/src/main/java/com/tang/game/GameApiApplication.java
@@ -2,8 +2,10 @@ package com.tang.game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class GameApiApplication {
 
   public static void main(String[] args) {
