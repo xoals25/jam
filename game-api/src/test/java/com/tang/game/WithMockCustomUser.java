2693ef7498583d6610e8217796b92ab69a52063e
xoals25 <xoals25@naver.com>
2023-05-28 12:23:26 +0900

    test: 유저 로그인 로그아웃 회원탈퇴 회원정보 가져오기 테스트 코드 작성 (Spring Security @AuthenticationPrincipal 테스트 null 문제)

---
+++ game-api/src/test/java/com/tang/game/WithMockCustomUser.java
@@ -0,0 +1,13 @@
+package com.tang.game;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithMockCustomUserSecurityContextFactory.class)
+public @interface WithMockCustomUser {
+  String userName() default "1";
+  String role() default "VALID";
+
+}
